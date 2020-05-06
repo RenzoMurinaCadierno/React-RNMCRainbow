3a22be1461fa9c99f8ca679db31d425914e73403
Renzo Nahuel Murina Cadierno <nmcadierno@hotmail.com>
2020-05-06 19:21:20 -0300

    Updates

---
+++ precache-manifest.2c60f664e9905ee00918e345e2521d21.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c3fffc2a67b928739b041d123f0be9f",
+    "url": "/React-RNMCRainbow/index.html"
+  },
+  {
+    "revision": "678d9f5303e223e8a825",
+    "url": "/React-RNMCRainbow/static/css/2.69b95c45.chunk.css"
+  },
+  {
+    "revision": "42e0ef7539a93e3afc78",
+    "url": "/React-RNMCRainbow/static/css/main.d70a901d.chunk.css"
+  },
+  {
+    "revision": "678d9f5303e223e8a825",
+    "url": "/React-RNMCRainbow/static/js/2.e78a5e8c.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/React-RNMCRainbow/static/js/2.e78a5e8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42e0ef7539a93e3afc78",
+    "url": "/React-RNMCRainbow/static/js/main.e804a9e5.chunk.js"
+  },
+  {
+    "revision": "37a8ef99ba408b9fad75",
+    "url": "/React-RNMCRainbow/static/js/runtime-main.a26cea3f.js"
+  },
+  {
+    "revision": "12a2a44849b9a42027cdff33f8cd54b0",
+    "url": "/React-RNMCRainbow/static/media/background.12a2a448.svg"
+  }
+]);
